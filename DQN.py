c56fcc6ef151712e01bb472b8cb7274ef58e2399
hbarkh <65371694+hbarkh@users.noreply.github.com>
2021-03-16 23:49:07 -0700

    Train Overnight
    Set params for overnight training of 1million episodes.

--- DQN.py
+++ DQN.py
@@ -24,23 +24,22 @@ env = Monitor(env, log_dir)
 #  actor (pi), value function (vf), Q function (qf)
 #  ex. net_arch=[dict(pi=[32, 32], vf=[32, 32])])
 #  to have networks share architecture use only net_arch
-policy_kwargs = dict(activation_fn=th.nn.GELU, net_arch=[256, 256, 256])
+policy_kwargs = dict(activation_fn=th.nn.GELU, net_arch=[512, 512, 512])
 
 # Set Hyper Parameters
-hyper_parameters = dict(learning_rate=0.001,
+hyper_parameters = dict(learning_rate=0.0001,
                         buffer_size=1000000,
-                        learning_starts=1000,
-                        batch_size=32,
+                        learning_starts=10000,
+                        batch_size=64,
                         tau=1.0, #1 for hard update
                         gamma=0.99,
                         train_freq=4,
                         gradient_steps=1,
-                        target_update_interval=10,
+                        target_update_interval=500,
                         exploration_initial_eps=1.0,
                         exploration_final_eps=0.005,
                         exploration_fraction=1,
                         max_grad_norm=10,
-                        tensorboard_log=None,
                         create_eval_env=False,
                         verbose=1,
                         seed=None,)
@@ -53,7 +52,7 @@ hyper_parameters = dict(learning_rate=0.001,
 model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs,**hyper_parameters)
 
 # Train Model
-model.learn(total_timesteps=100000, log_interval=4)
+model.learn(total_timesteps=50000000, log_interval=5)
 
 # Save Model
 model.save("LCA Env 7")
@@ -61,5 +60,6 @@ model.save("LCA Env 7")
 # Display results
 results_plotter.plot_results([log_dir],1e10, results_plotter.X_EPISODES, "LCA Env 7")
 plt.show()
+plt.savefig("50M steps figure.jpg")
 
 plot_results(log_dir) # Smoothed results
